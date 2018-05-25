198a0df7bdb8dca6fcfde0b7ce0a7e5e87bae0c1
Garett Breen <garett@redpeppersoftware.com>
2018-05-25 14:55:26 -0600

    Updates

--- Float/ValidationException.cs
+++ Float/ValidationException.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace Float
 {
     public class ValidationException : Exception
     {
-        public IEnumerable<ValidationError> ValidationErrors { get; set; }
-
-        public ValidationException()
-        {
-            ValidationErrors = new List<ValidationError>();
-        }
+        public IEnumerable<ValidationError> ValidationErrors { get; set; } = new List<ValidationError>();
 
         public override string Message => string.Join(Environment.NewLine, ValidationErrors.Select(x => $"{x.Field}: {x.Message}"));
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            var tempInfo = new SerializationInfo(GetType(), new FormatterConverter());
+            base.GetObjectData(tempInfo, context);
+            foreach (var entry in tempInfo)
+                if (entry.Name != "Message")
+                    info.AddValue(entry.Name, entry.Value, entry.ObjectType);
+                else
+                    info.AddValue("Message", Message);
+        }
     }
 
     public class ValidationError
